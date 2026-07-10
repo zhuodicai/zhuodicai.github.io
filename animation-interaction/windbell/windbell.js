aaa9c21fd8e4a3b83d3679d5e56eeb8e45b35888
zhuodicai <caizhuodia_life@yeah.net>
2026-07-10 01:06:33 -0700

    update

--- animation-interaction/windbell/windbell.js
+++ animation-interaction/windbell/windbell.js
@@ -1,5 +1,16 @@
 const ORIGINAL_TITLE = "Zhuodi Cai";
-const BELL_TITLE = "⁺⊹ 𓇢𓆸𓏲";
+const BELL_TITLES = [
+    "[:breeze?]",
+    "[:bird?]",
+    "[:leaf?]",
+    "[:petal?]",
+    "[:raindrop?]",
+    "[:butterfly?]",
+    "[:memory?]",
+    "[:dream?]",
+    "[:whisper?]",
+    "[:presence?]",
+];
 
 const sound = document.getElementById('windbell-sound');
 
@@ -11,18 +22,17 @@ sound.addEventListener('ended', () => {
 function restartGifAnimationAndSound() {
     const gifImage = document.querySelector('.windbell-animation');
 
-    // 获取当前的路径
+    // 获取当前路径
     const currentSrc = gifImage.src;
-
-    // 获取基础路径，不包含文件名
     const basePath = currentSrc.substring(0, currentSrc.lastIndexOf('/')) + '/';
 
-    // 重新加载GIF，触发动画
+    // 重新播放 GIF
     gifImage.src = '';
     gifImage.src = basePath + 'windbell.gif';
 
-    // 修改 tab 标题
-    document.title = BELL_TITLE;
+    // 随机选择一个标题
+    const randomTitle = BELL_TITLES[Math.floor(Math.random() * BELL_TITLES.length)];
+    document.title = randomTitle;
 
     // 从头播放音效
     sound.currentTime = 0;
