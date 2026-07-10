3f37c0c9c3e9e4bd12e4790297b7c75735e9f8fe
zhuodicai <caizhuodia_life@yeah.net>
2026-07-10 00:41:36 -0700

    update

--- animation-interaction/windbell/windbell.js
+++ animation-interaction/windbell/windbell.js
@@ -1,6 +1,15 @@
+const ORIGINAL_TITLE = "Zhuodi Cai";
+const BELL_TITLE = "⁺⊹ 𓇢𓆸𓏲";
+
+const sound = document.getElementById('windbell-sound');
+
+// 播放结束恢复标题
+sound.addEventListener('ended', () => {
+    document.title = ORIGINAL_TITLE;
+});
+
 function restartGifAnimationAndSound() {
     const gifImage = document.querySelector('.windbell-animation');
-    const sound = document.getElementById('windbell-sound');
 
     // 获取当前的路径
     const currentSrc = gifImage.src;
@@ -8,16 +17,19 @@ function restartGifAnimationAndSound() {
     // 获取基础路径，不包含文件名
     const basePath = currentSrc.substring(0, currentSrc.lastIndexOf('/')) + '/';
 
-    // 重新加载GIF，动态构建路径并重置src，触发动画
-    gifImage.src = ''; // 清空当前src
-    gifImage.src = basePath + 'windbell.gif'; // 重新设置src，触发动画
+    // 重新加载GIF，触发动画
+    gifImage.src = '';
+    gifImage.src = basePath + 'windbell.gif';
 
-    // 强制音效从头开始播放
-    sound.currentTime = 0;  // 设置音效的播放时间为0
-    sound.play();           // 播放音效
+    // 修改 tab 标题
+    document.title = BELL_TITLE;
+
+    // 从头播放音效
+    sound.currentTime = 0;
+    sound.play();
 }
 
-// 划过/点击 在GIF上触发音效
-document.querySelector('.windbell-animation').addEventListener('mouseover', restartGifAnimationAndSound);
-document.querySelector('.windbell-animation').addEventListener('click', restartGifAnimationAndSound);
-
+// 划过/点击 GIF 时触发
+const windbell = document.querySelector('.windbell-animation');
+windbell.addEventListener('mouseover', restartGifAnimationAndSound);
+windbell.addEventListener('click', restartGifAnimationAndSound);
